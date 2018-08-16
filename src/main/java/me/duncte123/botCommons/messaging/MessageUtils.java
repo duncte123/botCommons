23e06bdedec6230bb1670975f10ae0b07bf25520
Duncan Sterken <duncan.sterken123@gmail.com>
2018-08-16 08:38:48 +0200

    Move MessageUtils to botCommons

---
+++ src/main/java/me/duncte123/botCommons/messaging/MessageUtils.java
@@ -0,0 +1,324 @@
+/*
+ *    Copyright 2018 Duncan "duncte123" Sterken
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package me.duncte123.botCommons.messaging;
+
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.ErrorResponseException;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class MessageUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(MessageUtils.class);
+
+    private static final Consumer<Throwable> CUSTOM_QUEUE_ERROR = ex -> {
+        if (ex instanceof ErrorResponseException) {
+            if (((ErrorResponseException) ex).getErrorCode() != 10008) {
+                logger.error("RestAction queue returned failure", ex);
+                ex.printStackTrace();
+            }
+        }
+    };
+
+    /**
+     * This will react with a ❌ if the user doesn't have permission to run the command
+     *
+     * @param message the message to add the reaction to
+     */
+    public static void sendError(Message message) {
+        if (message.getChannelType() == ChannelType.TEXT) {
+            TextChannel channel = message.getTextChannel();
+            if (!channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_ADD_REACTION)) {
+                return;
+            }
+        }
+        message.addReaction("❌").queue(null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This method uses the sendError and sendMsg methods
+     *
+     * @param message the {@link Message} for the sendError method
+     * @param text    the {@link String} for the sendMsg method
+     */
+    public static void sendErrorWithMessage(Message message, String text) {
+        sendError(message);
+        new MessageBuilder().append(text).buildAll(MessageBuilder.SplitPolicy.NEWLINE).forEach(message1 ->
+                sendMsg(message.getTextChannel(), message1)
+        );
+    }
+    /**
+     * This will react with a ✅ if the user doesn't have permission to run the command
+     *
+     * @param message the message to add the reaction to
+     */
+    public static void sendSuccess(Message message) {
+        if (message.getChannelType() == ChannelType.TEXT) {
+            TextChannel channel = message.getTextChannel();
+            if (channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_ADD_REACTION)) {
+                message.addReaction("✅").queue(null, ignored -> {
+                });
+            }
+        }
+    }
+
+    /**
+     * This method uses the sendSuccess and sendMsg methods
+     *
+     * @param message the {@link Message} for the sendSuccess method
+     * @param text    the {@link String} for the sendMsg method
+     */
+    public static void sendSuccessWithMessage(Message message, String text) {
+        sendSuccess(message);
+        sendMsg(message.getTextChannel(), text);
+    }
+    /**
+     * This is a shortcut for sending formatted messages to a channel which also deletes it after delay unit
+     *
+     * @param event an instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param delay the {@link Long} that is our delay
+     * @param unit  the {@link TimeUnit} that is our unit that uses the delay parameter
+     * @param msg   the message format to send
+     */
+    public static void sendMsgAndDeleteAfter(GuildMessageReceivedEvent event, long delay, TimeUnit unit, String msg) {
+        sendMsgFormatAndDeleteAfter(event.getChannel(), delay, unit, StringUtils.abbreviate(msg, 2000), "");
+    }
+
+    /**
+     * This is a shortcut for sending formatted messages to a channel which also deletes it after delay unit
+     *
+     * @param tc    an instance of {@link TextChannel TextChannel}
+     * @param delay the {@link Long} that is our delay
+     * @param unit  the {@link TimeUnit} that is our unit that uses the delay parameter
+     * @param msg   the message format to send
+     */
+    public static void sendMsgAndDeleteAfter(TextChannel tc, long delay, TimeUnit unit, String msg) {
+        sendMsgFormatAndDeleteAfter(tc, delay, unit, StringUtils.abbreviate(msg, 2000), "");
+    }
+
+    /**
+     * This is a shortcut for sending formatted messages to a channel which also deletes it after delay unit
+     *
+     * @param event an instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param delay the {@link Long} that is our delay
+     * @param unit  the {@link TimeUnit} that is our unit that uses the delay parameter
+     * @param msg   the message format to send
+     * @param args  the arguments that should be used in the msg parameter
+     */
+    public static void sendMsgFormatAndDeleteAfter(GuildMessageReceivedEvent event, long delay, TimeUnit unit, String msg, Object... args) {
+        sendMsgFormatAndDeleteAfter(event.getChannel(), delay, unit, StringUtils.abbreviate(msg, 2000), args);
+    }
+
+    /**
+     * This is a shortcut for sending formatted messages to a channel which also deletes it after delay unit
+     *
+     * @param channel the {@link TextChannel TextChannel} that we want to send our message to
+     * @param delay   the {@link Long} that is our delay
+     * @param unit    the {@link TimeUnit} that is our unit that uses the delay parameter
+     * @param msg     the message format to send
+     * @param args    the arguments that should be used in the msg parameter
+     */
+    public static void sendMsgFormatAndDeleteAfter(TextChannel channel, long delay, TimeUnit unit, String msg, Object... args) {
+
+        sendMsg(channel, new MessageBuilder().append(String.format(StringUtils.abbreviate(msg, 2000), args)).build(),
+                it -> it.delete().reason("automatic remove").queueAfter(delay, unit, null, CUSTOM_QUEUE_ERROR)
+        );
+    }
+
+    /**
+     * This is a shortcut for sending formatted messages to a channel
+     *
+     * @param event an instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg   the message format to send
+     * @param args  the arguments that should be used in the msg parameter
+     */
+    public static void sendMsgFormat(GuildMessageReceivedEvent event, String msg, Object... args) {
+        sendMsg(event.getChannel(), (new MessageBuilder().append(String.format(StringUtils.abbreviate(msg, 2000), args)).build()), null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending formatted messages to a channel
+     *
+     * @param channel the {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message format to send
+     * @param args    the arguments that should be used in the msg parameter
+     */
+    public static void sendMsgFormat(TextChannel channel, String msg, Object... args) {
+        sendMsg(channel, (new MessageBuilder().append(String.format(StringUtils.abbreviate(msg, 2000), args)).build()), null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg   the message to send
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, String msg) {
+        sendMsg(event.getChannel(), (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event   a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg     the message to send
+     * @param success The success consumer
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, String msg, Consumer<Message> success) {
+        sendMsg(event.getChannel(), (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), success, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event   a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg     the message to send
+     * @param success The success consumer
+     * @param failure the failure consumer
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, String msg, Consumer<Message> success, Consumer<Throwable> failure) {
+        sendMsg(event.getChannel(), (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), success, failure);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     */
+    public static void sendMsg(TextChannel channel, String msg) {
+        sendMsg(channel, (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     * @param success The success consumer
+     */
+    public static void sendMsg(TextChannel channel, String msg, Consumer<Message> success) {
+        sendMsg(channel, (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), success, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     * @param success The success consumer
+     * @param failure the failure consumer
+     */
+    public static void sendMsg(TextChannel channel, String msg, Consumer<Message> success, Consumer<Throwable> failure) {
+        sendMsg(channel, (new MessageBuilder()).append(StringUtils.abbreviate(msg, 2000)).build(), success, failure);
+
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     * @deprecated Use  #sendEmbed(TextChannel, MessageEmbed)
+     */
+    @Deprecated
+    public static void sendMsg(TextChannel channel, MessageEmbed msg) {
+        sendMsg(channel, (new MessageBuilder()).setEmbed(msg).build(), null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg   the message to send
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, Message msg) {
+        sendMsg(event.getChannel(), msg, null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event   a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg     the message to send
+     * @param success The success consumer
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, Message msg, Consumer<Message> success) {
+        sendMsg(event.getChannel(), msg, success, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param event   a instance of {@link GuildMessageReceivedEvent GuildMessageReceivedEvent}
+     * @param msg     the message to send
+     * @param success The success consumer
+     * @param failure the failure consumer
+     */
+    public static void sendMsg(GuildMessageReceivedEvent event, Message msg, Consumer<Message> success, Consumer<Throwable> failure) {
+        sendMsg(event.getChannel(), msg, success, failure);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     */
+    public static void sendMsg(TextChannel channel, Message msg) {
+        sendMsg(channel, msg, null, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     * @param success The success consumer
+     */
+    public static void sendMsg(TextChannel channel, Message msg, Consumer<Message> success) {
+        sendMsg(channel, msg, success, CUSTOM_QUEUE_ERROR);
+    }
+
+    /**
+     * This is a shortcut for sending messages to a channel
+     *
+     * @param channel he {@link TextChannel TextChannel} that we want to send our message to
+     * @param msg     the message to send
+     * @param success The success consumer
+     * @param failure the failure consumer
+     */
+    public static void sendMsg(TextChannel channel, Message msg, Consumer<Message> success, Consumer<Throwable> failure) {
+        //Check if the channel exists
+        if ((channel != null && channel.getGuild().getTextChannelById(channel.getId()) != null) &&
+                channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_WRITE, Permission.MESSAGE_READ)) {
+            //Only send a message if we can talk
+            channel.sendMessage(msg).queue(success, failure);
+        }
+    }
+}
