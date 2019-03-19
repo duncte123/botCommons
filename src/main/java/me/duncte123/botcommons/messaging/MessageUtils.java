1a87c2af39ee8616d36b8b3d7383528a134be8f1
Duncan Sterken <duncan.sterken123@gmail.com>
2019-03-19 18:53:14 +0100

    Incorrect permission check:

--- src/main/java/me/duncte123/botcommons/messaging/MessageUtils.java
+++ src/main/java/me/duncte123/botcommons/messaging/MessageUtils.java
@@ -512,7 +512,7 @@ public class MessageUtils {
             MessageBuilder builder = new MessageBuilder(msg.getContentRaw());
 
             if (!msg.getEmbeds().isEmpty()) {
-                if (!channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_EMBED_LINKS)) {
+                if (channel.getGuild().getSelfMember().hasPermission(channel, Permission.MESSAGE_EMBED_LINKS)) {
                     builder.setEmbed(msg.getEmbeds().get(0));
                 } else {
                     msg.getEmbeds().forEach(
