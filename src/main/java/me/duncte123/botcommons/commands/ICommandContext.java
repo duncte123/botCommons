b846130067e2ec51cc3d7ee63b3c30a65fb90ec7
Duncan Sterken <duncan.sterken123@gmail.com>
2019-03-20 11:17:19 +0100

    Add ICommandContext

---
+++ src/main/java/me/duncte123/botcommons/commands/ICommandContext.java
@@ -0,0 +1,64 @@
+/*
+ *    Copyright 2019 Duncan "duncte123" Sterken
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package me.duncte123.botcommons.commands;
+
+import net.dv8tion.jda.bot.sharding.ShardManager;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+
+/**
+ * Dummy class that holds the basics for a command context
+ */
+public interface ICommandContext {
+
+    Guild getGuild();
+    GuildMessageReceivedEvent getEvent();
+
+    default TextChannel getChannel() {
+        return this.getEvent().getChannel();
+    }
+
+    default Message getMessage() {
+        return this.getEvent().getMessage();
+    }
+
+    default User getAuthor() {
+        return this.getEvent().getAuthor();
+    }
+
+    default Member getMember() {
+        return this.getEvent().getMember();
+    }
+
+    default JDA getJDA() {
+        return this.getEvent().getJDA();
+    }
+
+    default ShardManager getShardManager() {
+        return this.getJDA().asBot().getShardManager();
+    }
+
+    default User getSelfUser() {
+        return this.getJDA().getSelfUser();
+    }
+
+    default Member getSelfMember() {
+        return this.getGuild().getSelfMember();
+    }
+
+}
